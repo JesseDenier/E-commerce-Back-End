1b86ea92f25be5d828655a2bb4b962140f29b11d
Jesse <jessecdenier@gmail.com>
2024-04-03 14:59:10 -0500

    Added all routes for tags.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,6 @@ router.get("/:id", async (req, res) => {
 });
 
 // Creates a new category.
-// TODO: Get this to not return an error even though it works.
 router.post("/", async (req, res) => {
   try {
     const categoryData = await Category.create(req.body);
@@ -43,7 +42,6 @@ router.post("/", async (req, res) => {
 });
 
 // Updates an existing category by its id.
-// TODO: Make this work
 router.put("/:id", async (req, res) => {
   try {
     const categoryData = await Category.update(
